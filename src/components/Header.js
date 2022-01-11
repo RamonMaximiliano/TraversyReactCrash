6b62ca483dbf2645ba5c1dba83633569d3c1727a
Ramon <ramon_max1986@hotmail.com>
2022-01-11 18:04:59 -0300

    component

---
+++ src/components/Header.js
@@ -0,0 +1,17 @@
+//components should always have uppercase letter in the file
+
+import React from "react"
+
+const Header = () => {
+    return (
+        <header>
+            <h1>Task tracker</h1>
+
+        </header>
+    )
+}
+
+export default Header
+
+
+
